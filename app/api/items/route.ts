0a9982a04f11cb87f2c3a06e977417e280f9c2f8
hosan30 <50867996+ahfoysal@users.noreply.github.com>
2024-01-05 16:06:16 +0600

    Update route.ts

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -13,8 +13,7 @@ const items = {
       Id: "1ac400fd-98bd-46ee-b4e8-d2f983f2bbc2",
       Name: "Cake",
       Price: 11.0,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\Cake.jpg",
+      ImageUrl: "https://i.imgur.com/7hEb9ms.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
@@ -22,8 +21,7 @@ const items = {
       Id: "38332e95-7f66-45af-829b-c51b3d23caba",
       Name: "Chicken Fry",
       Price: 25.0,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\ChickenFry.jpg",
+      ImageUrl: "https://i.imgur.com/YyYut7G.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
@@ -31,8 +29,7 @@ const items = {
       Id: "39487b87-09e0-4e6c-acfa-6ac907e0742a",
       Name: "Fried Rice",
       Price: 9.0,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\FriedRice.jpg",
+      ImageUrl: "https://i.imgur.com/O0YSuFI.jpg",
       IsPopular: true,
       IsRecommended: false,
     },
@@ -40,8 +37,7 @@ const items = {
       Id: "a3ef9e36-c7ee-48c5-99e2-6f966157e116",
       Name: "Garlic Bread",
       Price: 24.75,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\GarlicBread.jpg",
+      ImageUrl: "https://i.imgur.com/dsqHvaZ.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
@@ -49,8 +45,7 @@ const items = {
       Id: "1280cb1b-9115-45c5-babd-601febd462de",
       Name: "Burger",
       Price: 10.99,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\burger.jpg",
+      ImageUrl: "https://i.imgur.com/5rzUi7f.jpg",
       IsPopular: true,
       IsRecommended: false,
     },
@@ -58,8 +53,7 @@ const items = {
       Id: "14f0a9d0-49f9-4020-a656-e23b7ac59216",
       Name: "Pizza",
       Price: 8.5,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\Pizza.jpg",
+      ImageUrl: "https://i.imgur.com/QVXpgTD.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
@@ -67,8 +61,7 @@ const items = {
       Id: "f6fdc153-052a-4eab-8b7d-bb1478d2179c",
       Name: "Pasta",
       Price: 3.0,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\Pasta.jpg",
+      ImageUrl: "https://i.imgur.com/Tl73dZE.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
@@ -76,8 +69,7 @@ const items = {
       Id: "2b1bca6f-f7d6-4854-86dd-126db70776a2",
       Name: "Cappuchino",
       Price: 3.0,
-      ImageUrl:
-        "http://www.api.technicaltest.quadtheoryltd.com/Resources/Image\\Cappuchino.jpg",
+      ImageUrl: "https://i.imgur.com/Tkzb5vn.jpg",
       IsPopular: true,
       IsRecommended: true,
     },
